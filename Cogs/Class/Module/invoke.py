e3e451c23f701bce6d6bc5af1879cc6659f2e7a3
onion <106968025+OnionKen1021@users.noreply.github.com>
2023-10-20 18:50:57 +0800

    Delete Cogs directory

--- Cogs/Class/Module/invoke.py
+++
@@ -1,7 +0,0 @@
-from Cogs.Class.ticket import Ticket
-
-def return_class(class_name):
-    match class_name:
-        case "Ticket":
-            return "from Cogs.Class.ticket import Ticket"
-        
